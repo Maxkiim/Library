5899fdeaae4e6bda2a19a60e22575e4b105e1671
Maxiikim <max97kim@gmail.com>
2026-03-11 14:37:10 -0700

    refactored Book obj to use class

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//icons
-lucide.createIcons();
-
 // Variables
 let myLibrary = [];
 const addBookBtn = document.querySelector('#submit-book');
@@ -8,14 +5,27 @@ const bookCards = document.querySelector('#book-cards');
 const dialog = document.querySelector('#book-dialog');
 const newBookBtn = document.querySelector('#new-book');
 
-// Objects
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID()
+// class for Book 
+
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+        this.id = crypto.randomUUID()
+    }
 }
+
+// call the class
+// Objects
+// function Book(title, author, pages, read){
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.id = crypto.randomUUID()
+// }
 
 // Functions
 
@@ -31,24 +41,43 @@ function displayBooks(){
         // Create Book Information Card
         const card = document.createElement('div');
         card.classList.add('card');
+        const cardInfo = document.createElement('div');
+        cardInfo.classList.add('card-info');
+        const removeBtnDiv = document.createElement('div');
+        removeBtnDiv.classList.add('remove-card');
         const title = document.createElement('h3');
         title.textContent = book.title;
         const author = document.createElement('p');
         author.classList.add('author');
-        author.textContent = book.author;
-        const pages = document.createElement('p');
-        pages.textContent = 'Pages:' + book.pages;
+        author.textContent = 'by ' + book.author;
+        const pagesBox = document.createElement('div');
+        const pages = document.createElement('h5');
+        pagesBox.classList.add('pagesBox');
+        pages.textContent = 'Pages:';
+        const pagesNum = document.createElement('p');
+        pagesNum.textContent = book.pages;
+        pagesBox.appendChild(pages);
+        pagesBox.appendChild(pagesNum);
         // Create a toggle button that shows if the book was read
-        const readBook = document.createElement('button');
-        readBook.textContent =  book.read ? 'Read' : 'Not Read';
-        readBook.addEventListener('click', () => {
+        const readBox = document.createElement('div');
+        readBox.classList.add('readBox');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = book.read;
+        const readStatus = document.createElement('p');
+        readStatus.textContent = book.read ? 'Read' : 'Not read yet';
+        checkbox.addEventListener('click', () => {
             book.toggleRead();
             displayBooks();
         });
+        readBox.appendChild(checkbox)
+        readBox.appendChild(readStatus)
+        // Create a remove book card button on each created book card
+        const removeDiv = document.createElement('div');
+        removeDiv.classList.add('remove-card');
         const removeBook = document.createElement('button');
-        // Create a remove book card on each created book card
         removeBook.classList.add('remove-btn');
-        removeBook.textContent = 'Remove Book';
+        removeBook.innerHTML = '<i data-lucide="trash-2"></i>';
         card.dataset.id = book.id //give each book card an id to then identify which card to delete
         // Give functionality to the Remove Card btn
         removeBook.addEventListener('click', () => {
@@ -56,12 +85,15 @@ function displayBooks(){
             displayBooks();
         })
         // Appending and Creating book card
-        card.appendChild(title);
-        card.appendChild(author);
-        card.appendChild(pages);
-        card.appendChild(readBook);
-        card.appendChild(removeBook);
+        cardInfo.appendChild(title);
+        cardInfo.appendChild(author);
+        cardInfo.appendChild(pagesBox);
+        cardInfo.appendChild(readBox);
+        removeDiv.appendChild(removeBook);
+        card.appendChild(cardInfo);
+        card.appendChild(removeDiv);
         bookCards.appendChild(card);
+        lucide.createIcons();
     });
 };
 function toggleDisplay(){
@@ -90,3 +122,7 @@ addBookBtn.addEventListener('click', (e) => {
     document.querySelector('#Pages').value = '';
     document.querySelector('#Read').checked = false;
 });
+
+
+//icons
+lucide.createIcons();
